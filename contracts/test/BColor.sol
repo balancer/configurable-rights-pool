100d73cc05b2ee7b59a6cf10d78ae7245f093392
Mike McDonald <mikeraymcdonald@gmail.com>
2020-07-25 10:17:28 -0500

    fix bcolor abstract for coverage

--- contracts/test/BColor.sol
+++ contracts/test/BColor.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.6.6;
 
-abstract contract BColor {
-    function getColor()
-        external view virtual
-        returns (bytes32);
-}
+// abstract contract BColor {
+//     function getColor()
+//         external view virtual
+//         returns (bytes32);
+// }
 
-contract BBronze is BColor {
+contract BBronze {
     function getColor()
-        external view override
+        external view
         returns (bytes32) {
             return bytes32("BRONZE");
         }
